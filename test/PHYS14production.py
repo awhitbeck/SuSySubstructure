bcdabbdb45cc4b54e6db900adaa783e7b051e5af
awhitbeck <whitbeck.andrew@gmail.com>
2015-05-12 18:53:16 -0500

    adding some more jet properties

--- test/PHYS14production.py
+++ test/PHYS14production.py
@@ -99,18 +99,25 @@ process.ak4JetsRaw = cms.EDProducer("fourVectorProducer",
                                     debug = cms.untracked.bool(False)
                                     )
 
-from AllHadronicSUSY.Utils.jetproperties_cfi import jetproperties
-process.JetsPropertiesRaw = jetproperties.clone(JetTag  = cms.InputTag('slimmedJets'),
-                                                BTagInputTag	        = cms.string('combinedInclusiveSecondaryVertexV2BJetTags'),
-                                                METTag  = cms.InputTag("slimmedMETs"),
-                                                )
+#from AllHadronicSUSY.Utils.jetproperties_cfi import jetproperties
+#process.JetsPropertiesRaw = jetproperties.clone(JetTag  = cms.InputTag('slimmedJets'),
+#                                                BTagInputTag	        = cms.string('combinedInclusiveSecondaryVertexV2BJetTags'),
+
+#                                                METTag  = cms.InputTag("slimmedMETs"),
+#                                                )
 
 
 process.TreeMaker2.VectorTLorentzVector.append("ak4Jets")
 process.TreeMaker2.VectorTLorentzVector.append("ak4JetsRaw")
 #process.TreeMaker2.VectorInt.append("GoodJets:passJetID(ak4Jets_passedJetID)")
 
-process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminator(ak4Jets_CSVdisc)")
+process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorUser(ak4Jets_CSVdisc)")
+process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorMVA(ak4Jets_MVAdisc)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorSimpleCSV(ak4Jets_SimpleCSVdisc)")
+
+#process.TreeMaker2.VectorInt.append("JetsProperties:NumBhadrons(ak4Jets_NumBhadrons)")
+#process.TreeMaker2.VectorInt.append("JetsProperties:NumChadrons(ak4Jets_NumChadrons)")
+
 process.TreeMaker2.VectorDouble.append("JetsProperties:chargedHadronEnergyFraction(ak4Jets_chargeHadEfrac)")
 process.TreeMaker2.VectorDouble.append("JetsProperties:neutralHadronEnergyFraction(ak4Jets_neutralHadEfrac)")
 process.TreeMaker2.VectorDouble.append("JetsProperties:photonEnergyFraction(ak4Jets_photonEfrac)")
@@ -119,15 +126,14 @@ process.TreeMaker2.VectorInt.append("JetsProperties:neutralHadronMultiplicity(ak
 process.TreeMaker2.VectorInt.append("JetsProperties:photonMultiplicity(ak4Jets_photonMult)")
 process.TreeMaker2.VectorInt.append("JetsProperties:flavor(ak4Jets_flavor)")
 
-process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:bDiscriminator(ak4JetsRaw_CSVdisc)")
-process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:chargedHadronEnergyFraction(ak4JetsRaw_chargeHadEfrac)")
-process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:neutralHadronEnergyFraction(ak4JetsRaw_neutralHadEfrac)")
-process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:photonEnergyFraction(ak4JetsRaw_photonEfrac)")
-process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:chargedHadronMultiplicity(ak4JetsRaw_chargedHadMult)")
-process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:neutralHadronMultiplicity(ak4JetsRaw_neutralHadMult)")
-process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:photonMultiplicity(ak4JetsRaw_photonMult)")
-process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:flavor(ak4JetsRaw_flavor)")
-
+#process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:bDiscriminator(ak4JetsRaw_CSVdisc)")
+#process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:chargedHadronEnergyFraction(ak4JetsRaw_chargeHadEfrac)")
+#process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:neutralHadronEnergyFraction(ak4JetsRaw_neutralHadEfrac)")
+#process.TreeMaker2.VectorDouble.append("JetsPropertiesRaw:photonEnergyFraction(ak4JetsRaw_photonEfrac)")
+#process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:chargedHadronMultiplicity(ak4JetsRaw_chargedHadMult)")
+#process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:neutralHadronMultiplicity(ak4JetsRaw_neutralHadMult)")
+#process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:photonMultiplicity(ak4JetsRaw_photonMult)")
+#process.TreeMaker2.VectorInt.append("JetsPropertiesRaw:flavor(ak4JetsRaw_flavor)")
 
 ### ak4 gen jets
 process.ak4GenJets = cms.EDProducer("fourVectorProducer",
@@ -183,7 +189,7 @@ process.WriteTree = cms.Path( process.Baseline *
 
                               process.genParticles * 
 
-                              process.JetsPropertiesRaw *
+                              #process.JetsPropertiesRaw *
                               process.ak4Jets *
                               process.ak4JetsRaw *
                               process.ak4GenJets *
