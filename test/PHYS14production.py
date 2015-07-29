72379f47edbea107a48cd526bd020b4d7dec66af
Kevin Pedro <kpedro88@gmail.com>
2015-07-29 12:43:30 -0500

    jet properties branches now stored by default in main treemaker cff, unneeded here

--- test/PHYS14production.py
+++ test/PHYS14production.py
@@ -4,12 +4,6 @@ from commandLineParameters import *
 process = cms.Process("analysis")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
-
-process.options   = cms.untracked.PSet(
-    SkipEvent   = cms.untracked.vstring('ProductNotFound'),
-    wantSummary = cms.untracked.bool(True)
-    )
 
 ## configure geometry & conditions
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
@@ -35,8 +29,8 @@ process = makeTreeFromMiniAOD(process,
                     reportfreq=options.reportEvery,
                     dataset=readFiles,
                     globaltag="PHYS14_25_V2::All",
-                    lostlepton=False,
-                    hadtau=False,
+                    lostlepton=True,
+                    hadtau=True,
                     tagandprobe=False,
                     numevents=options.numEvents,
                     doZinv=True,
@@ -46,6 +40,11 @@ process = makeTreeFromMiniAOD(process,
                     jsonfile="",
                     applyjec=False
                     )
+
+process.options   = cms.untracked.PSet(
+    SkipEvent   = cms.untracked.vstring('ProductNotFound'),
+    wantSummary = cms.untracked.bool(True)
+    )
 
 ## CONFIGURE TFILESERVICE
 
@@ -73,24 +72,24 @@ process.TreeMaker2.VarsDouble.append("fixedGridRhoFastjetAll(rho)")
 # ak4 jets
 # ==================
 
-process.TreeMaker2.VectorRecoCand.append("GoodJets(ak4Jets)")
-process.TreeMaker2.VectorRecoCand.append("slimmedJets(ak4JetsRaw)")
+#process.TreeMaker2.VectorRecoCand.append("GoodJets(ak4Jets)")
+#process.TreeMaker2.VectorRecoCand.append("slimmedJets(ak4JetsRaw)")
 #process.TreeMaker2.VectorInt.append("GoodJets:passJetID(ak4Jets_passedJetID)")
 
-process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorUser(ak4Jets_CSVdisc)")
-process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorMVA(ak4Jets_MVAdisc)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorUser(ak4Jets_CSVdisc)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorMVA(ak4Jets_MVAdisc)")
 #process.TreeMaker2.VectorDouble.append("JetsProperties:bDiscriminatorSimpleCSV(ak4Jets_SimpleCSVdisc)")
 
 #process.TreeMaker2.VectorInt.append("JetsProperties:NumBhadrons(ak4Jets_NumBhadrons)")
 #process.TreeMaker2.VectorInt.append("JetsProperties:NumChadrons(ak4Jets_NumChadrons)")
 
-process.TreeMaker2.VectorDouble.append("JetsProperties:chargedHadronEnergyFraction(ak4Jets_chargeHadEfrac)")
-process.TreeMaker2.VectorDouble.append("JetsProperties:neutralHadronEnergyFraction(ak4Jets_neutralHadEfrac)")
-process.TreeMaker2.VectorDouble.append("JetsProperties:photonEnergyFraction(ak4Jets_photonEfrac)")
-process.TreeMaker2.VectorInt.append("JetsProperties:chargedHadronMultiplicity(ak4Jets_chargedHadMult)")
-process.TreeMaker2.VectorInt.append("JetsProperties:neutralHadronMultiplicity(ak4Jets_neutralHadMult)")
-process.TreeMaker2.VectorInt.append("JetsProperties:photonMultiplicity(ak4Jets_photonMult)")
-process.TreeMaker2.VectorInt.append("JetsProperties:flavor(ak4Jets_flavor)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:chargedHadronEnergyFraction(ak4Jets_chargeHadEfrac)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:neutralHadronEnergyFraction(ak4Jets_neutralHadEfrac)")
+#process.TreeMaker2.VectorDouble.append("JetsProperties:photonEnergyFraction(ak4Jets_photonEfrac)")
+#process.TreeMaker2.VectorInt.append("JetsProperties:chargedHadronMultiplicity(ak4Jets_chargedHadMult)")
+#process.TreeMaker2.VectorInt.append("JetsProperties:neutralHadronMultiplicity(ak4Jets_neutralHadMult)")
+#process.TreeMaker2.VectorInt.append("JetsProperties:photonMultiplicity(ak4Jets_photonMult)")
+#process.TreeMaker2.VectorInt.append("JetsProperties:flavor(ak4Jets_flavor)")
 
 ### ak4 gen jets
 #process.TreeMaker2.VectorRecoCand.append("slimmedGenJets(ak4GenJets)")
